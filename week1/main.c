33539e31fc88d63ea1d0cce77afa9f5e71b94414
vedhitha97 <veds16cs@cmrit.ac.in>
2017-02-08 11:08:48 +0530

    Update main.c

---
+++ week1/main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+
+int add(int a[],int n) 
+{
+  	int sum = 0 ;
+    for(int i=0; i<n; i++)
+    {
+        sum += a[i] ;
+    }
+    return sum;
+}
+
+int main() 
+{
+    int n =10;
+    int a[10] = {1,2,3,4,5,6,7,8,9,0};
+    int res = add (a,n) ;
+    printf("Result is: %d \n", res);
+    return 0;
+}
