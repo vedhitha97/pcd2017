1833c4647ed45f73ab9c7784ceeb4bc627482c03
vedhitha97 <veds16cs@cmrit.ac.in>
2017-02-13 15:05:11 +0530

    Update main.c

--- week1/main.c
+++ week1/main.c
@@ -1,7 +1,7 @@
-#include <stdio.h>
 
+sum to add n numbers:
 
-
+#include<stdio.h>
 int add(int a[],int n) 
 {
   	int sum = 0 ;
@@ -20,3 +20,29 @@ int main()
     printf("Result is: %d \n", res);
     return 0;
 }
+sum to add two complex numbers:
+#include<stdio.h>
+typedef struct 
+{
+    float real;		
+    float imag;
+} complex;
+complex add (complex r1, complex r2)
+{
+    complex res;
+    res.real=r1.real+r2.real;
+    res.imag=r1.imag+r2.imag;
+    return res;
+}
+int main()
+{
+    complex r1={4,6};
+    complex r2={3,8};
+    complex r3=add(r1,r2);
+    printf("result:%f+i.%f\n",r3.real,r3.imag);
+    return 0;
+}
+          
+          
+          
+          
